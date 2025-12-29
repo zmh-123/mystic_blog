633d8009d31e4ea2022a6d87d6e4c6dae806276b
zmh <123@zmh.com>
2025-12-29 21:15:20 +0800

    Site Update: 2025-12-29 21:15:19

--- up.ps1
+++ up.ps1
@@ -1,19 +1,19 @@
 # 1. 停止如果出现错误
 $ErrorActionPreference = "Stop"
 
-Write-Host "?? 开始清理旧缓存..." -ForegroundColor Cyan
+Write-Host " 开始清理旧缓存..." -ForegroundColor Cyan
 hexo clean
 
-Write-Host "?? 开始生成静态网页..." -ForegroundColor Cyan
+Write-Host " 开始生成静态网页..." -ForegroundColor Cyan
 hexo g
 
-Write-Host "?? 正在提交到 GitHub..." -ForegroundColor Cyan
+Write-Host " 正在提交到 GitHub..." -ForegroundColor Cyan
 git add .
 # 获取当前时间作为提交备注
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 git commit -m "Site Update: $date"
 
-Write-Host "?? 正在推送到云端 (Vercel 会自动捕获)..." -ForegroundColor Cyan
+Write-Host " 正在推送到云端 (Vercel 会自动捕获)..." -ForegroundColor Cyan
 git push
 
-Write-Host "? 恭喜！发布成功！" -ForegroundColor Green
+Write-Host " 恭喜！发布成功！" -ForegroundColor Green
